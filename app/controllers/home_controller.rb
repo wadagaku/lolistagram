66271ab9e0380b73087df2d4aa0e0a5673e1dd0b
wadagaku <obusumagaku.1005@gmail.com>
2015-08-19 15:12:23 +0000

    first commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
     def index
-        @medias = Instagram.tag_recent_media(URI.encode("cute"))
+        @medias = Instagram.tag_recent_media(URI.encode("ナイトプール"))
     end
+    
+    def free
+        keyword = params[:key]
+        @medias = Instagram.tag_recent_media(URI.encode(keyword),{:count => 50})          
+    end   
 end
